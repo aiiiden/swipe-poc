eccf10007437bc9dc7a8cacf3ea78bf3d9433947
aiiiden <hjjam100@gmail.com>
2024-10-20 22:33:09 +0900

    fix route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Layout from './components/layout';
 import { lazyLoad } from './lib/route-utils';
+import IndexPage from './pages';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        lazy: lazyLoad('pages/index'),
+        element: <IndexPage />,
       },
       {
         path: '/about',
